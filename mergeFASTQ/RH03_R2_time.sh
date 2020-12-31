640a03af0f2d36728ab38728f2b7ef26ae3b8ad0
Xin Zhou <34285739+xzhou7@users.noreply.github.com>
2020-12-31 12:41:47 -0800

    Add files via upload

---
+++ mergeFASTQ/RH03_R2_time.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Start_combining_files"
+date +"%T"
+
+cat /Volumes/Xin_Lab/Single\ Cell\ Raw\ file/190726_COOPER_0285_AH2J2WBBXY_L1/RH03/Fastqs/*R2_001.fastq.gz /Volumes/Xin_Lab/Single\ Cell\ Raw\ file/190731_COOPER_0287_AH2GHFBBXY_L1/RH03/Fastqs/*R2_001.fastq.gz /Volumes/Xin_Lab/Single\ Cell\ Raw\ file/190731_COOPER_0287_AH2GHFBBXY_L2/RH03/Fastqs/*R2_001.fastq.gz /Volumes/Xin_Lab/Single\ Cell\ Raw\ file/190828_COOPER_0290_AH2HF5BBXY_L1/RH03/Fastqs/*R2_001.fastq.gz /Volumes/Xin_Lab/Single\ Cell\ Raw\ file/190828_COOPER_0290_AH2HF5BBXY_L2/RH03/Fastqs/*R2_001.fastq.gz /Volumes/Xin_Lab/Single\ Cell\ Raw\ file/190828_COOPER_0290_AH2HF5BBXY_L3/RH03/Fastqs/*R2_001.fastq.gz /Volumes/Xin_Lab/Single\ Cell\ Raw\ file/190828_COOPER_0290_AH2HF5BBXY_L4/RH03/Fastqs/*R2_001.fastq.gz /Volumes/Xin_Lab/Single\ Cell\ Raw\ file/190828_COOPER_0290_AH2HF5BBXY_L8/RH03/Fastqs/*R2_001.fastq.gz > /Volumes/Xin_Lab/SingleCell_GEO_PBMC/RH03/RH03_R2_001.fastq.gz
+
+echo "file_moving_finished"
+date +"%T"
---
+++ mergeFASTQ/RH04_4_R2_time.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Start_combining_files"
+date +"%T"
+
+cat /Volumes/Xin_Lab/Single\ Cell\ Raw\ file/190726_COOPER_0285_AH2J2WBBXY_L1/RH04-4/Fastqs/*R2_001.fastq.gz /Volumes/Xin_Lab/Single\ Cell\ Raw\ file/190731_COOPER_0287_AH2GHFBBXY_L1/RH04-4/Fastqs/*R2_001.fastq.gz /Volumes/Xin_Lab/Single\ Cell\ Raw\ file/190731_COOPER_0287_AH2GHFBBXY_L2/RH04-4/Fastqs/*R2_001.fastq.gz /Volumes/Xin_Lab/Single\ Cell\ Raw\ file/190828_COOPER_0290_AH2HF5BBXY_L1/RH04-4/Fastqs/*R2_001.fastq.gz /Volumes/Xin_Lab/Single\ Cell\ Raw\ file/190828_COOPER_0290_AH2HF5BBXY_L2/RH04-4/Fastqs/*R2_001.fastq.gz /Volumes/Xin_Lab/Single\ Cell\ Raw\ file/190828_COOPER_0290_AH2HF5BBXY_L3/RH04-4/Fastqs/*R2_001.fastq.gz /Volumes/Xin_Lab/Single\ Cell\ Raw\ file/190828_COOPER_0290_AH2HF5BBXY_L4/RH04-4/Fastqs/*R2_001.fastq.gz /Volumes/Xin_Lab/Single\ Cell\ Raw\ file/190828_COOPER_0290_AH2HF5BBXY_L8/RH04-4/Fastqs/*R2_001.fastq.gz > /Volumes/Xin_Lab/SingleCell_GEO_PBMC/RH04-4/RH04-4_R2_001.fastq.gz
+
+echo "file_moving_finished"
+date +"%T"
